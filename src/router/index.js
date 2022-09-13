5598376c4e07d820c307249cfae70272338b185d
lvjingxin <1768538073@qq.com>
2022-09-13 15:07:35 +0800

    构建登录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Layout from '@/views/Layout'
+import Login from '@/views/Login'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    component: Layout
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Login/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <van-form @submit="onSubmit">
+  <van-field
+    v-model="form.username"
+    name="用户名"
+    label="用户名"
+    placeholder="用户名"
+    :rules="[{ required: true, message: '请填写用户名' }]"
+  />
+  <van-field
+    v-model="form.password"
+    type="password"
+    name="密码"
+    label="密码"
+    placeholder="密码"
+    :rules="[{ required: true, message: '请填写密码' }]"
+  />
+  <div style="margin: 16px;">
+    <van-button round block type="info" native-type="submit">提交</van-button>
+  </div>
+</van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login-vue',
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('打印啦提交啦')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
