31694fa928d031dddba16fdc982f879779c5be18
lvjingxin <1768538073@qq.com>
2022-09-19 14:25:21 +0800

    优化按需引入

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from 'vant'// 引入组件vant
+import '@/utils/vants'// 按需引入
+// import Vant from 'vant'// 引入组件vant
 import 'vant/lib/index.css'// 引入组件样式vant
 import 'amfe-flexible'// 引入移动端适配插件 rem方案
 // import { fetchAllChannels } from '@/api/index.js'// 引入方法
@@ -11,7 +12,7 @@ import 'amfe-flexible'// 引入移动端适配插件 rem方案
 //   console.log(res)
 // }
 // getChannels()
-Vue.use(Vant)// 使用组件vant
+// Vue.use(Vant)// 使用组件vant
 Vue.config.productionTip = false
 
 new Vue({
